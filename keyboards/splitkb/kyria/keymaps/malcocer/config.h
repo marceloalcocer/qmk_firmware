e4c95fbc8349bae52c59bedc9f2db8eb27272ef1
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2021-10-10 22:05:47 +0200

    Add kyria malcocer keymap

---
+++ keyboards/splitkb/kyria/keymaps/malcocer/config.h
@@ -0,0 +1,37 @@
+/* Copyright 2021 Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+#endif
+
+// Set handedness by EEPROM
+// N.b. Flash EEPROM using dfu-split-{left,right} target. Try not to do this too much though
+// https://docs.qmk.fm/#/config_options?id=setting-handedness
+#define EE_HANDS
+
+// Tweak encoder settings
+#define ENCODER_DIRECTION_FLIP
+#define ENCODER_DIRECTION_FLIP_RIGHT
+
+// Tweak tapping term
+#define TAPPING_TERM 150
+
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
+//#define SPLIT_USB_DETECT
+//#define NO_USB_STARTUP_CHECK
