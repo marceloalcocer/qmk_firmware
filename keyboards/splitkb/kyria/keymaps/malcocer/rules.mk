1fa711143c440c9b13c9d935292cd949c2d18048
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2022-08-15 23:04:10 +0200

    Add mouse keys

--- keyboards/splitkb/kyria/keymaps/malcocer/rules.mk
+++ keyboards/splitkb/kyria/keymaps/malcocer/rules.mk
@@ -1,14 +1,14 @@
 # OLED display #################################################################
-OLED_ENABLE = yes               # Enable OLED displays
-OLED_DRIVER = SSD1306           # Specify OLED driver
+OLED_ENABLE = yes                               # Enable OLED displays
+OLED_DRIVER = SSD1306                           # Specify OLED driver
 
 # Encoders #####################################################################
-ENCODER_ENABLE = yes            # Enable encoders
+ENCODER_ENABLE = yes                            # Enable encoders
 
 # Trackball ####################################################################
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pimoroni_trackball
+POINTING_DEVICE_ENABLE = yes                    # Enable pointing device
+POINTING_DEVICE_DRIVER = pimoroni_trackball     # Specify pointing device driver
 
 # Misc #########################################################################
-RGBLIGHT_ENABLE = no            # Disable RGB underglow
-MOUSEKEY_ENABLE = no            # Disable mouse keys
+RGBLIGHT_ENABLE = no                            # Disable RGB underglow
+MOUSEKEY_ENABLE = yes                           # Enable mouse keys
